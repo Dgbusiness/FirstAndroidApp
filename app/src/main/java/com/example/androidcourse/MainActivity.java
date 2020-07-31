8f02ddbbf82e44ad0e94b3a630ac912404e1bd65
Luis David Garcia Lunar <ldgl1215@gmail.com>
2020-07-31 11:38:37 -0400

    optimizando codigo

--- app/src/main/java/com/example/androidcourse/MainActivity.java
+++ app/src/main/java/com/example/androidcourse/MainActivity.java
@@ -2,33 +2,49 @@ package com.example.androidcourse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText text;
+    private Button minusBtn;
+    private Button addBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = findViewById(R.id.valTxt);
+        minusBtn = findViewById(R.id.minusBtn);
+        addBtn = findViewById(R.id.addBtn);
+        minusBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                minusOne();
+            }
+        });
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addOne();
+            }
+        });
     }
 
 
-    public void addOne(View view) {
+    public void addOne() {
         // Suma 1 al textview
-        EditText text = (EditText) findViewById(R.id.valTxt);
         int value = Integer.parseInt(text.getText().toString());
         ++value;
         text.setText(String.valueOf(value));
 
     }
 
-    public void minusOne(View view) {
+    public void minusOne() {
         // Resta 1 al textview
-        EditText text = (EditText) findViewById(R.id.valTxt);
         int value = Integer.parseInt(text.getText().toString());
         --value;
         text.setText(String.valueOf(value));
